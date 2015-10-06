e04e7ab9ecb3adbc5ae85ef068bde79ef4c9dd3a
Michael Joyce <ubermichael@gmail.com>
2015-10-06 15:22:24 -0700

    Move the terms history into a controller.

---
+++ src/AppBundle/Controller/TermOfUseHistoryController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\TermOfUseHistory;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * TermOfUseHistory controller.
+ *
+ * @Route("/termhistory")
+ */
+class TermOfUseHistoryController extends Controller
+{
+
+    /**
+     * Lists all TermOfUseHistory entities.
+     *
+     * @Route("/", name="termhistory")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $dql = 'SELECT e FROM AppBundle:TermOfUseHistory e ORDER BY e.termId';
+        $query = $em->createQuery($dql);
+        $paginator = $this->get('knp_paginator');
+        $entities = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            25
+        );
+
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a TermOfUseHistory entity.
+     *
+     * @Route("/{id}", name="termhistory_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:TermOfUseHistory');
+        $query = $repo->createQueryBuilder('h')
+                ->where('h.termId = :termId')
+                ->getQuery()
+                ->setParameter('termId', $id);
+
+        $paginator = $this->get('knp_paginator');
+        $entities = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            25
+        );
+
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+}
