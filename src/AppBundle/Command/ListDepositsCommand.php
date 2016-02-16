1a2f142d2a768f6c4a9004c5a2574f21be3601d8
Michael Joyce <ubermichael@gmail.com>
2016-02-15 17:15:55 -0800

    some useful commands.

---
+++ src/AppBundle/Command/ListDepositsCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\DepositRepository;
+use Monolog\Registry;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Dump\Container;
+use Symfony\Component\HttpKernel\Tests\Logger;
+
+/**
+ * Reset the processing status for one deposit.
+ */
+class ListDepositsCommand extends ContainerAwareCommand {
+
+    /**
+     * @var Registry
+     */
+    protected $em;
+
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    /**
+     * Set the service container, and initialize the command.
+     *
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null) {
+        parent::setContainer($container);
+        $this->logger = $container->get('monolog.logger.processing');
+        $this->em = $container->get('doctrine')->getManager();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function configure() {
+        $this->setName('pln:list');
+        $this->setDescription('List deposits.');
+        $this->addArgument(
+                'state', InputArgument::REQUIRED, 'List deposits in this state'
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        /** @var DepositRepository $repo */
+        $repo = $this->em->getRepository('AppBundle:Deposit');
+
+        $state = $input->getArgument('state');
+		$deposits = $repo->findBy(array('state' => $state));
+        foreach($deposits as $deposit) {
+			$output->writeln("{$deposit->getJournal()->getUuid()}/{$deposit->getDepositUuid()}");
+        }
+    }
+
+}
---
+++ src/AppBundle/Command/SummarizeDepositsCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\DepositRepository;
+use Monolog\Registry;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Dump\Container;
+use Symfony\Component\HttpKernel\Tests\Logger;
+
+/**
+ * Reset the processing status for one deposit.
+ */
+class SummarizeDepositsCommand extends ContainerAwareCommand {
+
+    /**
+     * @var Registry
+     */
+    protected $em;
+
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    /**
+     * Set the service container, and initialize the command.
+     *
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null) {
+        parent::setContainer($container);
+        $this->logger = $container->get('monolog.logger.processing');
+        $this->em = $container->get('doctrine')->getManager();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function configure() {
+        $this->setName('pln:summary');
+        $this->setDescription('Summarize deposits.');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        /** @var DepositRepository $repo */
+        $repo = $this->em->getRepository('AppBundle:Deposit');
+		$summary = $repo->stateSummary();
+		$count = 0;
+		foreach($summary as $row) {
+			$output->writeln(sprintf("%6d - %s", $row['ct'], $row['state']));
+			$count += $row['ct'];
+		}
+		$output->writeln(sprintf("%6d - total", $count));
+    }
+
+}
