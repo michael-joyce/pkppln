3a2c242c3212c068c69d5f4fd42dd823afee104d
Michael Joyce <ubermichael@gmail.com>
2016-03-09 14:34:38 -0800

    well, that was dumb.

--- src/AppBundle/Command/Shell/ExtractDepositCommand.php
+++ src/AppBundle/Command/Shell/ExtractDepositCommand.php
@@ -116,7 +116,7 @@ class ExtractDepositCommand extends ContainerAwareCommand {
             $offset = 1; // xpath string offsets start at 1, not zero.
             while($offset < $length) {
 				$end = $offset+$chunkSize;
-				$chunk = $xp->evaluate("substring(./text(), {$offset}, {$end})", $embedded);				
+				$chunk = $xp->evaluate("substring(./text(), {$offset}, {$chunkSize})", $embedded);				
                 fwrite($fh, base64_decode($chunk));
                 $offset = $end;
                 $output->write('.');
