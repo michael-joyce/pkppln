ca2d07f094ae2d5b8d4ef69606a3e3f0267c2fc9
Michael Joyce <ubermichael@gmail.com>
2016-04-28 14:42:48 -0700

    more tests.

--- test/functional/AppBundle/AppBundle/Controller/SwordControllerTest.php
+++ test/functional/AppBundle/AppBundle/Controller/SwordControllerTest.php
@@ -3,10 +3,14 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Utility\AbstractTestCase;
+use Symfony\Component\BrowserKit\Client;
 use Symfony\Component\HttpFoundation\Response;
 
 class SwordControllerTest extends AbstractTestCase {
 
+	/**
+	 * @var Client
+	 */
 	protected $client;
 
 	public function setUp() {
@@ -14,9 +18,42 @@ class SwordControllerTest extends AbstractTestCase {
 		$this->client = static::createClient();
 	}
 	
+    public function fixtures() {
+        return array(
+            'AppBundle\DataFixtures\ORM\test\LoadJournals',
+        );
+    }
+    
+	public function testServiceDocument() {
+		$this->client->request('GET', '/api/sword/2.0/sd-iri', array(), array(), array(
+			'HTTP_On-Behalf-Of' => '7AD045C9-89E6-4ACA-8363-56FE9A45C34F',
+			'HTTP_Journal-Url' => 'http://example.com'
+		));
+		$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+	}
+	
 	public function testServiceDocumentNoOBH() {
-		$this->client->request('GET', '/api/sword/2.0/sd-iri');
+		$this->client->request('GET', '/api/sword/2.0/sd-iri', array(), array(), array(
+			'HTTP_Journal-Url' => 'http://example.com'
+		));
+		$this->assertEquals(400, $this->client->getResponse()->getStatusCode());
+	}
+	
+	public function testServiceDocumentNoJournalUrl() {
+		$this->client->request('GET', '/api/sword/2.0/sd-iri', array(), array(), array(
+			'HTTP_On-Behalf-Of' => '7AD045C9-89E6-4ACA-8363-56FE9A45C34F',
+		));
 		$this->assertEquals(400, $this->client->getResponse()->getStatusCode());
 	}
 
+	public function testServiceDocumentBadHeaders() {
+		$this->client->request('GET', '/api/sword/2.0/sd-iri', array(), array(), array(
+			'HTTP_On-Behalf-Of' => '',
+			'HTTP_Journal-Url' => ''
+		));
+		$this->assertEquals(400, $this->client->getResponse()->getStatusCode());
+	}
+	
+	
+	
 }
