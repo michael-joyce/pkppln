247ef9d3c155a7a1686f12820c5c2ef6cef9529b
Michael Joyce <ubermichael@gmail.com>
2016-05-30 07:23:07 -0700

    Start testing the shell commands.

---
+++ test/AppBundle/Command/Shell/ExtractDepositCommandTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Command\Shell;
+
+use AppBundle\Utility\AbstractCommandTestCase;
+
+class ExtractDepositCommandTest extends AbstractCommandTestCase {
+	
+	
+	public function getCommand() {
+		
+	}
+
+	public function getCommandName() {
+		
+	}
+
+}
